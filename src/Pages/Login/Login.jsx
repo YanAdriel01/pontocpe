c6eed6ecfa900fc537c797ac30b1441afa9794cd
yanadriel <yanadrieel@gmail.com>
2025-11-04 00:59:08 -0300

    responsividade na pagina de login

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,51 @@
+
+import {    Header, 
+            Titulo,
+            InputWraper,
+            InputString,
+            CaptionWraper,
+            Caption,
+            ButtonWraper,
+            Button } from "../../Components/Components";
+import { PasswordInput } from "../../Components/PasswordInput.jsx";
+import logo_cpe from "../../Images/logo_cpe.png"
+import { useNavigate } from "react-router-dom";
+
+export default function Login(){
+
+
+    const navigate = useNavigate();
+
+    return(
+        <>
+            <Header>
+
+                <a href="/" className="icon_cpe">
+                    <img src={logo_cpe} alt="Consultoria de Projetos Elétricos" className="logo_cpe"/>
+                </a>
+
+            </Header>
+
+            <Titulo>Login</Titulo>
+
+            <InputWraper>
+                <InputString placeholder="Email" type="email" />
+                <PasswordInput placeholder="Senha" />
+            </InputWraper>
+
+            <CaptionWraper>
+
+                <Caption>Não tem login? Faça seu cadastro 
+                    <a href="/cadastro">aqui</a>
+                </Caption>
+
+            </CaptionWraper>
+
+            <ButtonWraper>
+                <Button
+                    onClick={() => navigate("/")}
+                >ENTRAR</Button>
+            </ButtonWraper>
+        </>
+    )
+}
